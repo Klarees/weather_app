4689fb14f89bd586a0b3c7b513825fcff76ad355
Klarees <dizonclariskimmy@gmail.com>
2022-12-26 14:54:40 +0800

    added config module

---
+++ config.js
@@ -0,0 +1,8 @@
+const constants = {
+    openWeatherMap: {
+        BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY: "fe88fea180c588cf833d34577c8c9ba5"
+    }
+}
+
+module.exports = constants;
